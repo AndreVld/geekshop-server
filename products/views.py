9d6135f1a0761d73922766dac8369e82e67718f8
AndreyIotko <andrey.iotko@bk.ru>
2021-07-03 00:55:39 +0300

    convert to dynamic file display

--- products/views.py
+++ products/views.py
@@ -7,7 +7,7 @@ MODULE_DIR = os.path.dirname(__file__)
 
 def index(request):
     context = {
-        'title': 'GeekShop'
+        'title': 'GeekShop',
     }
     return render(request, 'products/index.html', context)
 
@@ -20,4 +20,4 @@ def products(request):
     file_path = os.path.join(MODULE_DIR, 'fixtures/products.json')
     context['products'] = json.load(open(file_path, encoding="utf-8"))
 
-    return render(request, 'products/products.html')
+    return render(request, 'products/products.html', context)
