08fd847e54e4b76ada08c673113a45b7f654910c
AndreyIotko <andrey.iotko@bk.ru>
2021-07-11 06:38:29 +0300

    added a mechanism for editing user information in the personal account.

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 def login(request):
@@ -37,6 +37,19 @@ def registration(request):
         'form': form,
     }
     return render(request, 'users/registration.html', context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, files=request.FILES, data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Все измененеия сохранены.')
+            return HttpResponseRedirect(reverse('users:profile'))
+
+    form = UserProfileForm(instance=request.user)
+    context = {'title': 'GeekShop - Личный кабинет', 'form': form, }
+    return render(request, 'users/profile.html', context)
 
 
 def logout(request):
