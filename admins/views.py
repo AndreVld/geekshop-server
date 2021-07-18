2e23e0f170e83627eb589c65c2786fa17df8510f
AndreyIotko <andrey.iotko@bk.ru>
2021-07-18 13:11:12 +0300

    added 'update,delete' for users

--- admins/views.py
+++ admins/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 from users.models import User
+from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 
 
 def index(request):
@@ -8,13 +10,46 @@ def index(request):
 
 
 def admin_users(request):
-    context = {'users': User.objects.all()}
+    context = {
+        'title': 'Ползователи',
+        'users': User.objects.all()}
     return render(request, 'admins/admin-users-read.html', context)
 
 
 def admin_users_create(request):
-    return render(request, 'admins/admin-users-create.html')
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminRegistrationForm()
+    context = {
+        'title': 'Создание ползователя',
+        'form': form,
+    }
+    return render(request, 'admins/admin-users-create.html', context)
 
 
-def admin_users_update_delete(request):
-    return render(request, 'admins/admin_users_update_delete.html', context)
+def admin_users_update(request, pk):
+    selected_user = User.objects.get(id=pk)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(instance=selected_user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+
+    form = UserAdminProfileForm(instance=selected_user)
+    context = {
+        'title': 'Редактирование ползователя',
+        'form': form,
+        'selected_user': selected_user,
+    }
+    return render(request, 'admins/admin-users-update-delete.html', context)
+
+
+def admin_users_remove(request, pk):
+    user = User.objects.get(id=pk)
+    user.is_active = False
+    user.save()
+    return HttpResponseRedirect(reverse('admins:admin_users'))
