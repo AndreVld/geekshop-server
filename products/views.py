cc8ee5966887f6f34c0c41a9df8b33b1eeaf3249
AndreyIotko <andrey.iotko@bk.ru>
2021-07-20 08:16:03 +0300

    added product filtering by category

--- products/views.py
+++ products/views.py
@@ -10,10 +10,12 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request):
+def products(request, category_id=None):
     context = {
         'title': 'GeekShop - Каталог',
         'products': Product.objects.all(),
         'categories': ProductCategory.objects.all(),
     }
+    if category_id:
+        context['products'] = Product.objects.filter(category_id=category_id)
     return render(request, 'products/products.html', context)
