2a274d8d6431f3f08aa3c8c3b239cc54cb046b51
AndreyIotko <andrey.iotko@bk.ru>
2021-09-13 17:24:59 +0300

    added tests

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o#f#*peju$cym)p0i%fv$zmit5r4vdo1p2j)^@g(8&pq3_ct6@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
